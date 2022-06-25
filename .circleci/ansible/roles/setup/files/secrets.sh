32380d96211ee72cff022b47a2d48800dcf599be
Williams <billmal071@gmail.com>
2022-06-25 23:04:24 +0100

    feat: update frontend

---
+++ .circleci/ansible/roles/setup/files/secrets.sh
@@ -0,0 +1,11 @@
+export TYPEORM_CONNECTION=postgres
+export TYPEORM_MIGRATIONS_DIR=./src/migrations
+export TYPEORM_ENTITIES=./src/modules/domain/**/*.entity.ts
+export TYPEORM_MIGRATIONS=./src/migrations/*.ts
+export TYPEORM_HOST=udapeople.cemahe3gmk7p.us-east-1.rds.amazonaws.com
+export TYPEORM_PORT=5432
+export TYPEORM_USERNAME=postgres
+export TYPEORM_PASSWORD=rocket14
+export TYPEORM_DATABASE=postgres
+export NODE_ENV=production
+export ENVIRONMENT=production
